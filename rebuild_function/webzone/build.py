fcfdae2e5bee38540aae47c105aa065359436cae
Austin Adams <git@austinjadams.com>
2023-01-09 02:55:57 -0500

    Don't send Hugo a random arg that blows its mind

--- rebuild_function/webzone/build.py
+++ rebuild_function/webzone/build.py
@@ -4,4 +4,6 @@ def clone_shallow(clone_url, repo_path):
     subprocess.run(['git', 'clone', '--depth', '1', clone_url, repo_path], check=True)
 
 def hugo_build(repo_path, hugo_out_path):
-    subprocess.run(['hugo', '--verbose', '--buildFuture', '--destination', hugo_out_path, repo_path], cwd=repo_path, check=True)
+    # Use --buildFuture because we probably shouldn't trust the local time of
+    # this system (doing so has caused serious confusion for me in the past)
+    subprocess.run(['hugo', '--verbose', '--buildFuture', '--destination', hugo_out_path], cwd=repo_path, check=True)
