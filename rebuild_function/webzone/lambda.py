eae9059e4efcf75d842a7942c9598dbc4edb036c
Austin Adams <git@austinjadams.com>
2023-01-09 12:43:15 -0500

    Take my CS degree away from me

--- rebuild_function/webzone/lambda.py
+++ rebuild_function/webzone/lambda.py
@@ -18,7 +18,7 @@ def lambda_handler(event, context):
     #   sha256=9cc57f2ca39c2d81aed7e3d82af0b5711863bd3403bb8f024c4c3b4ecf9652a4
     their_hmac = event['headers']['x-hub-signature-256'].split('=', 1)[-1].lower()
     our_hmac = hmac.new(webhook_secret.encode('utf-8'), event['body'].encode('utf-8'), 'sha256').hexdigest()
-    if hmac.compare_digest(their_hmac, our_hmac)
+    if hmac.compare_digest(their_hmac, our_hmac):
         raise ValueError('access denied!')
 
     if json.loads(event['body'])['ref'] != 'refs/heads/master':
